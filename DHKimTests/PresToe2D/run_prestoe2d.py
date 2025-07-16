7a46a09baf8d41e7c60ae1abd6a8379329b78982
DHKim <robot.dhkim@gmail.com>
2025-07-16 09:33:42 -0400

    joint idx

--- DHKimTests/PresToe2D/run_prestoe2d.py
+++ DHKimTests/PresToe2D/run_prestoe2d.py
@@ -6,7 +6,7 @@ import numpy as np
 # add argparse arguments
 parser = argparse.ArgumentParser(description="Tutorial on using the differential IK controller.")
 parser.add_argument("--robot", type=str, default="prestoe2d", help="Name of the robot.")
-parser.add_argument("--num_envs", type=int, default=5, help="Number of environments to spawn.")
+parser.add_argument("--num_envs", type=int, default=2, help="Number of environments to spawn.")
 # append AppLauncher cli args
 AppLauncher.add_app_launcher_args(parser)
 # parse the arguments
@@ -63,7 +63,8 @@ def run_simulator(sim: sim_utils.SimulationContext, scene: InteractiveScene):
     # note: we only do this here for readability.
     robot = scene["robot"]
 
-    robot_entity_cfg = SceneEntityCfg("robot")
+    robot_entity_cfg = SceneEntityCfg("robot", joint_names=[".*_hip", ".*_knee", ".*_ankle"])
+    # robot_entity_cfg = SceneEntityCfg("robot", joint_names=["l_hip", "l_knee", "l_ankle"])
     robot_entity_cfg.resolve(scene)
 
     # Define simulation stepping
@@ -82,16 +83,24 @@ def run_simulator(sim: sim_utils.SimulationContext, scene: InteractiveScene):
             robot.reset()
 
         # # apply actions 
-        # curr_time = count * sim_dt
-        # joint_pos_des = 0.5 + 0.5 * np.sin(2*np.pi*curr_time)
-        # robot.set_joint_position_target(joint_pos_des, joint_ids=robot_entity_cfg.joint_ids)
+        curr_time = count * sim_dt
+        # 0, 1, 2: x, y, pitch
+        # 3, 4: hip (left, right)
+        # 5, 6: knee (left, right)
+        # 7, 8: ankle (left, right)
+
+        # print(f"joint id: {robot_entity_cfg.joint_ids}")
+        # print(f"joint pos: {robot.data.joint_pos[:, robot_entity_cfg.joint_ids]}")
+
+        joint_pos_des = 0.5 + 0.5 * np.sin(2*np.pi*curr_time)
+        robot.set_joint_position_target(joint_pos_des, joint_ids=[7, 8])
 
         # # set joint torque
         # jtorque_des = 50. * np.sin(2*np.pi*curr_time)
-        # # robot.set_joint_effort_target(jtorque_des, joint_ids=robot_entity_cfg.joint_ids)
+        # robot.set_joint_effort_target(jtorque_des, joint_ids=robot_entity_cfg.joint_ids)
  
         # robot.set_joint_position_target(joint_pos_des, joint_ids=robot_entity_cfg.joint_ids)
-        # scene.write_data_to_sim()
+        scene.write_data_to_sim()
         # perform step
         sim.step()
         # update sim-time
